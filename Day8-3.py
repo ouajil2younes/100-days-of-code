59dfc6dd14506e84801188ea3aea3ee929763bdb
Ouajil Younes <younesyouness212@gmail.com>
2025-11-26 16:37:43 +0100

    encode game with no fonction

--- Day8-3.py
+++ Day8-3.py
@@ -1,21 +1,45 @@
 origin=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j','k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
  'u', 'v', 'w', 'x', 'y', 'z']
-word_to_enc=input('enter the word that u want to encode\n')
-encoded_list=[]
-encode_by=int(input('enter te number that u wont to use for the encode'))
-#get the id of the position of the letter that the user entred
-for j in range(len(word_to_enc)) :
-    for i in range(len(origin)):
-        if word_to_enc[j]==origin[i]:
-            if i+encode_by<=25:
-                print(f'i={i}  encode_by={encode_by}')
-                print(f'we got i+encode_by<=26 {origin[i+encode_by]}')
-                encoded_list.append(origin[i+encode_by])
-            elif i+encode_by>25:
-                count=(encode_by+i)%26
-                print(f'-------- i={i} \n count={count} \n  encode_by={encode_by}')
-                encoded_list.append(origin[count])
-                print(f'we got  i+encode_by>26 {origin[count]}')
-
-print(f'resolt {encoded_list}')
-    
+play=True
+while play:
+    choisc=input('do you want to encode type \'e\' and if you want to decode enter \'d\':\n')
+    if choisc=='e':
+        word_to_enc=input('enter the message that you want to encode:\n')
+        encoded_list=[]
+        encode_by=int(input('entre your shift number \n'))
+        #get the id of the position of the letter that the user entred
+        for j in range(len(word_to_enc)) :
+            for i in range(len(origin)):
+                if word_to_enc[j]==origin[i]:
+                    if i+encode_by<=25:
+                        encoded_list.append(origin[i+encode_by])
+                    elif i+encode_by>25:
+                        count=(encode_by+i)%26
+                        encoded_list.append(origin[count])
+        resolt=''.join(encoded_list)
+        print(f'yor encoded message is {resolt} ')
+        play_chois=input('do you want to repeat again or exit pres \'y\' to continu or \'n\' to exit \n')
+        if play_chois=='n':
+            play=False
+        elif play_chois=='y':
+            play=True
+    if choisc=='d':
+        word_to_enc=input('enter the message that you want to encode:\n')
+        encoded_list=[]
+        encode_by=int(input('entre your shift number \n'))
+        #get the id of the position of the letter that the user entred
+        for j in range(len(word_to_enc)) :
+            for i in range(len(origin)):
+                if word_to_enc[j]==origin[i]:
+                    if i+encode_by<=25:
+                        encoded_list.append(origin[i-encode_by])
+                    elif i+encode_by>25:
+                        count=(encode_by-i)%26
+                        encoded_list.append(origin[count])
+        resolt=''.join(encoded_list)
+        print(f'yor encoded message is {resolt} ')
+        play_chois=input('do you want to repeat again or exit pres \'y\' to continu or \'n\' to exit \n')
+        if play_chois=='n':
+            play=False
+        elif play_chois=='y':
+            play=True
