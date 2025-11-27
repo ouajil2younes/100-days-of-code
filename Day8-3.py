b79b94097822dc7b6ef454e58200c8b15ebfb3dd
Ouajil Younes <younesyouness212@gmail.com>
2025-11-27 17:13:55 +0100

    full correct game

--- Day8-3.py
+++ Day8-3.py
@@ -2,35 +2,28 @@ origin=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j','k', 'l', 'm', 'n', 'o'
  'u', 'v', 'w', 'x', 'y', 'z']
 origin2=origin[::-1]
 play=True
-def encrypt(origin):
-    word_to_enc=input('enter the message that you want to encode:\n')
+def start_enc_dec(origin):
+    word_to_enc=input('enter your message :\n')
     encoded_list=[]
     encode_by=int(input('entre your shift number \n'))
     for j in range(len(word_to_enc)) :
             for i in range(len(origin)):
-                if word_to_enc[j]==origin[i]:
+                if word_to_enc[j] not in origin:
+                    print(f'{word_to_enc[j]} is not in {origin}')
+                    encoded_list.append(word_to_enc[j])
+                    break
+                elif word_to_enc[j]==origin[i]:
                     if i+encode_by<=25:
                         encoded_list.append(origin[i+encode_by])
+                        break
                     elif i+encode_by>25:
                         count=(encode_by+i)%26
                         encoded_list.append(origin[count])
+                        break
+
     resolt=''.join(encoded_list)
-    print(f'yor encoded message is {resolt} ')
-def decrypt(origin):
-    word_to_enc=input('enter the message that you want to encode:\n')
-    encoded_list=[]
-    encode_by=int(input('entre your shift number \n'))
- #get the id of the position of the letter that the user entred
-    for j in range(len(word_to_enc)) :
-            for i in range(len(origin)):
-                if word_to_enc[j]==origin[i]:
-                    if i+encode_by<=25:
-                        encoded_list.append(origin[i+encode_by])
-                    elif i+encode_by>25:
-                        count=(encode_by+i)%26
-                        encoded_list.append(origin[count])
-    resolt=''.join(encoded_list)
-    print(f'yor encoded message is {resolt} ')
+    print(f'yor resolt is {resolt} ')
+
 def play_again():
         play_chois=input('do you want to repeat again or exit pres \'y\' to continu or \'n\' to exit \n')
         if play_chois=='n':
@@ -40,8 +33,8 @@ def play_again():
 while play:
     choisc=input('do you want to encode type \'e\' and if you want to decode enter \'d\':\n')
     if choisc=='e':
-        encrypt(origin=origin)
+        start_enc_dec(origin=origin)
         play=play_again()
     if choisc=='d':
-        decrypt(origin=origin2)
+        start_enc_dec(origin=origin2)
         play=play_again()
